707b7a5e42e6f949858fca5b8716225c75ee8dd5
Vinicius88088 <vinicius_oliveira42@estudante.sesisenai.org.br>
2023-11-21 20:00:29 -0300

    Questão11_TempoDViagem

---
+++ Exercícios/Exercício02/Questão11/TempoDeViagem.py
@@ -0,0 +1,6 @@
+Distancia = float (input ("Insira a distância percorrida: "))
+Velocidade = float (input ("Insira a velocidade média: "))
+
+TempoViagem = Distancia / Velocidade
+
+print ("O tempo de viagem estimado é de:", TempoViagem, "Hrs")
