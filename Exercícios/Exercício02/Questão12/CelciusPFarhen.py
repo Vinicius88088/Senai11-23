cb34faff478d57ae33c5ef701b4c3f69a8249649
vinicius88088 <vinicius_oliveira42@estudante.sesisenai.org.br>
2023-11-27 22:28:50 -0300

    Questão12-CelciusPFahrenheit

---
+++ Exercícios/Exercício02/Questão12/CelciusPFarhen.py
@@ -0,0 +1,7 @@
+print ("\n\n\nInsira o valor em Celcius para converter para Fahrenheit\n")
+
+ValorCelcius = float (input ("Valor em Celcius: "))
+
+ValorFahrenheit = (1.8 * ValorCelcius) + 32
+
+print ("\n\nO valor em Fahrenheit é: ", ValorFahrenheit, "\n\n\n")
